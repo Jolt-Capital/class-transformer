c5f428a0a9a950047ad7aed082fc4d2e57f55b1f
meunierfrederic <frederic.meunier@jolt-capital.com>
2024-09-26 11:23:18 +0200

    polyfill and jest config and tsconfig

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,9 @@
 module.exports = {
-  // preset: 'ts-jest',
-  // testEnvironment: 'node',
   // collectCoverageFrom: ['src/**/*.ts', '!src/**/index.ts', '!src/**/*.interface.ts'],
   testEnvironment: 'node',
+  setupFilesAfterEnv: ['./test/setupTests.ts'],
   transform: {
-    '^.+.tsx?$': ['ts-jest', { tsconfig: 'tsconfig.spec5.json' }],
+    '^.+.tsx?$': ['ts-jest', { tsconfig: 'tsconfig.spec.json' }],
   },
   // globals: {
   //   'ts-jest': {
