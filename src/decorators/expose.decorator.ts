779d2f59eefcbd04d4d521dd038f0b3fc175a89e
Frederic Meunier <frederic.meunier@jolt-capital.com>
2024-09-30 17:42:52 +0200

    some tests

--- src/decorators/expose.decorator.ts
+++ src/decorators/expose.decorator.ts
@@ -1,5 +1,6 @@
 import { defaultMetadataStorage } from '../storage';
 import {
+  ClassConstructor,
   ExcludeMetadata,
   ExposeMetadata,
   ExposeOptions,
@@ -8,6 +9,8 @@ import {
   TypeMetadata,
 } from '../interfaces';
 import { FieldMetadata, getFieldMetadata } from './commons.decorator';
+import { Type, TypeForMap, TypeForSet } from './type.decorator';
+import { stringify } from 'querystring';
 
 /**
  * Marks the given class or property as included. By default the property is included in both
@@ -45,4 +48,62 @@ export function Expose(options: ExposeOptions = {}) {
       defaultMetadataStorage.addExposeMetadata(exposeMetadata);
     }
   };
+}
+
+function Expose2<This, Value>(exposedType: ClassConstructor<Value>, options: ExposeOptions = {}) {
+  return function (target: This, context: ClassFieldDecoratorContext<This, Value>): void {
+    if (context.kind === 'field') {
+      const fieldMetadata: FieldMetadata = getFieldMetadata(context, context.name);
+      fieldMetadata.expose = { options };
+    } else if (context.kind === 'class') {
+      const exposeMetadata: ExposeMetadata = { target: target as Function, propertyName: undefined, options: options };
+      defaultMetadataStorage.addExposeMetadata(exposeMetadata);
+    }
+  };
+}
+
+class User {
+  kind: 'user';
+}
+
+class Weapon {
+  kind: 'weapon';
+}
+
+// Avant
+class TestExpose {
+  @Expose() aString: string;
+
+  @Expose() aNumber: number;
+
+  @Expose() aBoolean: boolean;
+
+  @Type(() => User) aUser: User;
+
+  @Type(() => User) someUsers: User[];
+}
+
+// Après
+class TestExpose2 {
+  @Expose2(String) aString: string;
+
+  @Expose2(Number) aNumber: number;
+
+  @Expose2(Boolean) aBoolean: boolean;
+
+  @Expose2(String) anInvalidProperty: boolean;
+
+  @Expose2(User) aUser: User;
+
+  @Expose2(Weapon) anotherInvalidProperty: User;
+
+  @Type(() => User) arrayOfUsers: Array<User>;
+  @TypeForMap(() => User) mapOfUsers: Map<string, User>;
+  @TypeForSet(() => User) setOfUsers: Set<User>;
+  @TypeForMap(() => User) invalidSetProperty: Set<User>;
+
+  // ou mieux (compile errors car pas encore implémenté)
+  @Type(() => Array<User>, () => User) anotherArrayOfUsers: User[];
+  @Type(() => Map<string, User>, () => User) anotherMapOfUsers: Map<string, User>;
+  @Type(() => Set<User>, () => User) anotherSetOfUsers: Map<string, User>;
 }
